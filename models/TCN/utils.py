55f0fb8f89967598c67593b0f174b470d9a678f6
joshuawe <joshua.wendland@tum.de>
2023-04-15 17:17:20 +0200

    add validation split dataloader

--- models/TCN/utils.py
+++ models/TCN/utils.py
@@ -9,7 +9,8 @@ from .fast_data_loader import FastDataLoader
 
 def get_data_loader_synthetic(
             path: str, 
-            batch_size: int, 
+            batch_size: int,
+            splits: list,
             drop_time: bool=True
             ) -> "tuple[FastDataLoader, int, torch.Tensor]":
     """
@@ -21,6 +22,8 @@ def get_data_loader_synthetic(
         Path to the dataset. Is expected to be in EAV format, not wide.
     batch_size : int
         Number of samples per batch.
+    splits : list
+        List of splits to use. Should be a list of 3 floats, summing to 1. Train, val, test. Note: Only the first two are used and the remaining is used for test.
     drop_time : bool, optional
         Should time be exluded from the input features, by default True
 
@@ -32,29 +35,36 @@ def get_data_loader_synthetic(
     
     df = pd.read_csv(path, compression="gzip")
     df = pd.pivot_table(df, index=["id", "time"], columns="variable", values="value").reset_index(level=[0, 1])
+    
+    # impute and fill all missing values with 0
+    df = df.groupby('id', group_keys=False).apply(lambda x: x.ffill().fillna(value=0))
     if drop_time == True:
         df = df.drop(columns=["time"])
     print(df.shape)
     grouped = df.groupby("id")
     input_features = len(df.columns) - 2
+    num_samples = len(grouped)
     print(df.columns, '\n')
     print(df.tail(), '\n')
-    print('Number of samples: ', len(grouped))
+    print('Number of samples: ', num_samples)
 
     # Get batches
     batch_representation = np.stack([i[1].iloc[:,1:] for n, i in enumerate(grouped)])
     batch_representation = batch_representation.astype(np.float64)
 
     # get dataloader
-    dataloader = FastDataLoader(batch_representation, batch_size, 1)
-    print("Batch.shape: ", next(iter(dataloader)).shape)
-    input_features = next(iter(dataloader)).shape[-1] -1  # -1 for the label
+    train_num = int(splits[0] * num_samples)
+    dataloader_train = FastDataLoader(batch_representation[:train_num], batch_size, 1)
+    dataloader_val = FastDataLoader(batch_representation[train_num:], batch_size, 1)
+    print('Careful, only using the first two splits for train and val, no test split is used!')
+    print("Batch.shape: ", next(iter(dataloader_train)).shape)
+    input_features = next(iter(dataloader_train)).shape[-1] -1  # -1 for the label
     
     # get class imbalance factor
     prop_cases = df['Y_ts'].mean()
     pos_weight = torch.tensor((1 - prop_cases) / prop_cases)
     print('pos_weight: ', pos_weight)
-    return dataloader, input_features, pos_weight
+    return dataloader_train, dataloader_val, input_features, pos_weight
 
 
 
